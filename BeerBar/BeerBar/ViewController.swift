5a4bb86e52d3b615434a81e6c89154526f06d6e8
Alexander Kononok <alexanderkononok@MacBook-Air-Alexander.local>
2020-09-12 16:15:32 +0300

    add clear project

---
+++ BeerBar/BeerBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BeerBar
+//
+//  Created by Alexander Kononok on 9/12/20.
+//  Copyright © 2020 Alexander Kononok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
